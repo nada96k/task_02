9936786a0287352550d305b77a565968b9746004
Your Namenada96k <nadawe_96@hotmail.com>
2019-09-30 19:07:28 +0300

    test passed

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def my_website(request):
+	context = {  
+          "msg":"Hello World!"
+	}
+	return render(request,'mypage.html',context)
